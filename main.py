94e84d033601fb5c96aed2e28b3050581d6bd329
Gopal24krishna <gopal.krishna@leapcodes.com>
2026-08-31 11:01:00 +0530

    .

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ load_dotenv()
 print("OpenAI Key Loaded:", bool(os.getenv("OPENAI_API_KEY")))
 print("AWS Region:", os.getenv("AWS_REGION"))
 print("DB Name:", os.getenv("DB_NAME"))
+#just for testing 
